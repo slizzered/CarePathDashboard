a96815fbaddb464942557bb563e05b35d1cfcadb
Mieke Möller <mieke.moeller56@gmail.com>
2023-04-11 13:58:30 +0200

    made some CSS styling

--- frontend/src/components/Table/Table.js
+++ frontend/src/components/Table/Table.js
@@ -11,9 +11,16 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
  * Use of this source code is governed by an MIT-style license that can be found in the LICENSE file at https://mui.com/x/introduction/licensing/
  */
 
-function createData(beginRT, patient, blocking_task, progress_overview) {
+function createData(
+  beginRT,
+  treatment_task,
+  patient,
+  blocking_task,
+  progress_overview
+) {
   return {
     beginRT,
+    treatment_task,
     patient,
     blocking_task,
     progress_overview,
@@ -36,7 +43,8 @@ function Row(props) {
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </div>
-        <div className={Styles.table_cell}>{row.beginRT}</div>
+        <div className={Styles.table_cell_begin_rt}>{row.beginRT}</div>
+        <div className={Styles.table_cell}>{row.treatment_task}</div>
         <div className={Styles.table_cell}>{row.patient}</div>
         <div className={Styles.table_cell}>{row.blocking_task}</div>
         <div className={Styles.table_cell}>{row.progress_overview}</div>
@@ -50,29 +58,32 @@ function Row(props) {
 
 const rows = [
   createData(
-    "2h erste RT Hyperarc",
+    "2h",
+    "erste RT Hyperarc",
     "Olaf",
     "OARs einzeichnen",
     "in Bearbeitung",
     4.0
   ),
-  createData("5h erste RT", "Ralf", "PD vorbereiten", "Offen", 4.3),
+  createData("5h", "erste RT", "Ralf", "PD vorbereiten", "Offen", 4.3),
   createData(
-    "5h RT Boost",
+    "5h",
+    "RT Boost",
     "Manuela",
     "PTV einzeichnen",
     "in Bearbeitung",
     6.0
   ),
-  createData("10h erste RT", "Fritz", "Arzt Freigabe", "Verfügbar", 4.3),
-  createData("12h erste RT", "Maike", "PTV anpassen", "in Bearbeitung", 3.9),
+  createData("10h", "erste RT", "Fritz", "Arzt Freigabe", "Verfügbar", 4.3),
+  createData("12h", "erste RT", "Maike", "PTV anpassen", "in Bearbeitung", 3.9),
 ];
 
 export default function CollapsibleTable() {
   return (
     <div className={Styles.table_container}>
       <div className={Styles.table_header}>
-        <div className={Styles.table_cell} />
+        <div className={Styles.table_cell}/>
+        <div className={Styles.table_cell}/>
         <div className={Styles.table_cell}> Behandlungsbeginn </div>
         <div className={Styles.table_cell}>Patient(-ID)</div>
         <div className={Styles.table_cell}>Blocking Task</div>
