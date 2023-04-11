597baece43b697af8c5002a41cf81278469b4e64
Carlchristian Eckert <Carli-Eckert@gmx.de>
2023-04-11 21:53:54 +0200

    added some small caching improvements and some CORS details

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
 import cachetools
 import sqlite3
 import time
@@ -13,17 +16,22 @@ mockup_db.create_mockupPatient('mydb.sqlite', random_seed=5)
 
 # connect to local database file
 conn_local = sqlite3.connect('mydb.sqlite')
-cache = cachetools.TTLCache(maxsize=100, ttl=3)
+cache = cachetools.TTLCache(maxsize=100, ttl=15)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex='https?://localhost:8*',
+    allow_credentials=True,
+    allow_methods=['GET'],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
-    now = cache.get("now")
-    if now is None:
-        now = time.ctime()
-        cache["now"] = now
-    return f'''the following API Endpoints are available:<br>
+    now = time.ctime()
+    return f'''{now}<br>the following API Endpoints are available:<br>
             <a href='/ignoredPatients'>ignoredPatients</a><br>
             <a href='/ersteRTsMockup'>ersteRTsMockup</a>'''
 
@@ -33,11 +41,22 @@ async def get_ignored_patients():
     cursor = conn_local.execute('''SELECT * FROM IgnorePatient''')
     return await sqlite_tools.cursor_to_json(cursor)
 
+
 @app.get("/ersteRTsMockup")
 async def get_first_rts_mockup():
-    cursor = conn_local.execute('''SELECT * FROM MockupPatient''')
-    return await sqlite_tools.cursor_to_json(cursor)
+    patient_table = cache.get("mockup_patient_table")
+    if patient_table is None:
+        cursor = conn_local.execute('''SELECT * FROM MockupPatient''')
+        patient_table = await sqlite_tools.cursor_to_json(cursor)
+        cache["mockup_patient_table"] = patient_table
+    return patient_table
 
 
-
-
+@app.get("/oncologist_timetable/{oncologist_name}")
+async def get_oncologist_timetable(oncologist_name: str):
+    cursor = conn_local.execute('''SELECT TOP 1 * FROM ''')
+    result = cursor.fetchone()
+    if len(result) == 0:
+        pass
+    else:
+        return result
